c0c08d84787d654c44b4b17d859dc295a13622bb
gaomeng1900 <gaomeng1900@gmail.com>
2022-11-14 21:20:43 +0800

    support

--- examples/support/index.ts
+++ examples/support/index.ts
@@ -1,2 +1,21 @@
 export {}
-console.log('hello.')
+
+const adapter = (await navigator.gpu.requestAdapter())!
+
+l(adapter)
+
+adapter.features.forEach((k) => l('feature', k))
+
+const device = await adapter.requestDevice({ label: 'haha' })
+
+device.lost.then(() => l('device lost'))
+
+l(device)
+
+device.features.forEach((k) => l('feature', k))
+
+//
+
+function l(...args) {
+	console.log(...args)
+}
